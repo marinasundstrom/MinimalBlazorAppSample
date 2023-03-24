e9be31cdea85f222ae3ca90f7a67ab44b734537a
Marina Sundström <marna.li@outlook.com>
2023-03-24 18:32:19 +0100

    Enable nullable as errors

--- Server/Pages/Error.cshtml.cs
+++ Server/Pages/Error.cshtml.cs
@@ -13,7 +13,7 @@ namespace BlazorApp1.Server.Pages
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
+        public string RequestId { get; set; } = default!;
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
