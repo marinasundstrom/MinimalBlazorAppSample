a6777f6ecbfeab5d32709b63baa5332080d7746f
Marina Sundström <marna.li@outlook.com>
2023-04-24 19:22:45 +0200

    Simplify

--- Server/ApiVersions.cs
+++ Server/ApiVersions.cs
@@ -6,11 +6,11 @@ public static class ApiVersions
 {
     private static ApiVersion[]? _versions;
 
-    public static readonly ApiVersion V1 = new ApiVersion(1, 0);
+    public static readonly ApiVersion V1 = new (1, 0);
 
-    public static readonly ApiVersion V2 = new ApiVersion(2, 0);
+    public static readonly ApiVersion V2 = new (2, 0);
 
-    public static IEnumerable<ApiVersion> Versions => _versions ?? (_versions = new ApiVersion[] {
+    public static IEnumerable<ApiVersion> Versions => _versions ?? (_versions = new [] {
             V1,
             V2
         });
