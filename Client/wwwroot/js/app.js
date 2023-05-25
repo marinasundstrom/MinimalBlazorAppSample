58533f8bd85a2b1099bec51b4c524e748fc9fa7e
Marina Sundström <marna.li@outlook.com>
2023-05-25 21:42:14 +0200

    Splashscreen color scheme

--- Client/wwwroot/js/app.js
+++ Client/wwwroot/js/app.js
@@ -1,5 +1,24 @@
-﻿
-window.blazorCulture = {
+﻿window.blazorCulture = {
     get: () => window.localStorage['BlazorCulture'],
     set: (value) => window.localStorage['BlazorCulture'] = value
 };
+
+window.isDarkModePreferred = () => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return true;
+    }
+    return false;
+};
+
+function splashscreen() {
+    const userPreferences = JSON.parse(window.localStorage["userPreferences"] ?? "null");
+    const preferredColorScheme = userPreferences?.DarkLightTheme;
+    const colorScheme = preferredColorScheme ?? (window.isDarkModePreferred() ? 2 : 1);
+
+    if (colorScheme == 2) {
+        const elem = document.getElementById("splashscreen");
+        elem.classList.toggle("dark");
+    }
+}
+
+splashscreen();
